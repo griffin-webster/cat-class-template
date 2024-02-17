671e243673baf96094ae2f38fe24edf1b8b27e99
griffin-webster <griffin.webster@students.fhu.edu>
2024-02-16 21:18:35 -0600

    initial commit

--- cat.py
+++ cat.py
@@ -1,6 +1,19 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__(self):
+        self.name - "Unknown"
+        self.age = 0
+
+    def speak(self):
+        return "Meow"
+
+stella = Cat()
+stella.name = "Stella"
+stella.age = 7
+
+garfield = Cat()
+garfield.name = "Garfield"
+garfield.age = 50
 
 
 #Create objects here
